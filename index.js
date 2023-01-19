0d89b5fcada19c6e8d7071480aec45464e665058
ANetinho7 <aurelinoneto10@hotmail.com>
2023-01-18 22:14:48 -0300

    teste

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var config = {
 
     notify_on_logout: true,
     notify_on_initialization: true,
-    ping: [true, "@doener"],
+    ping: [true, "@everyone"],
 
     embed: {
         username: "A.Netinho7",
